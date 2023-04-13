fa4160a1009af4f16a27a23b5a71e6009fc6e282
Gabriel Cardoso de Avelar <gabri22kocebrac@gmail.com>
2023-04-13 20:08:48 -0300

    [feat] add funcionario/employee model

---
+++ src/models/Funcionario.js
@@ -0,0 +1,33 @@
+import { Model, DataTypes } from 'sequelize';
+
+class Funcionario extends Gerente {
+  static init(sequelize) {
+    super.init({
+      dataNascimento: {
+        type: DataTypes.DATEONLY,
+        allowNull: false,
+        validate: {
+          notNull: {
+            msg: 'A data de nascimento é obrigatória'
+          },
+          notEmpty: {
+            msg: 'A data de nascimento não pode ser vazia'
+          },
+          isDate: {
+            msg: 'A data de nascimento deve estar no formato YYYY-MM-DD'
+          }
+        }
+      },
+      foto: {
+        type: DataTypes.BLOB('long'),
+        allowNull: true
+      }
+    }, {
+      sequelize,
+      modelName: 'funcionario',
+      tableName: 'funcionarios'
+    });
+  }
+}
+
+export { Funcionario };
