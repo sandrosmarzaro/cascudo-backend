7f9433a6c44b3fef59bc1aa7330296e84c3265a5
Gabriel Cardoso de Avelar <gabri22kocebrac@gmail.com>
2023-04-14 16:04:31 -0300

    [fix] add flag field to indicate manager

--- src/models/Funcionario.js
+++ src/models/Funcionario.js
@@ -1,8 +1,40 @@
-import { DataTypes } from 'sequelize';
+//Responsável: Gabriel Cardoso.
+import { Model, DataTypes } from 'sequelize';
 
-class Funcionario extends Gerente {
+class Funcionario extends Model {
     static init(sequelize) {
         super.init({
+            nome: {
+                type: DataTypes.STRING,
+                allowNull: false,
+                validate: {
+                    notNull: {
+                        msg: 'O nome é obrigatório'
+                    },
+                    notEmpty: {
+                        msg: 'O nome não pode ser vazio'
+                    },
+                    len: {
+                        args: [1, 100],
+                        msg: 'O nome deve ter entre 1 e 100 caracteres'
+                    }
+                }
+            },
+            codigo: {
+                type: DataTypes.STRING,
+                allowNull: false,
+                unique: {
+                    msg: 'Já existe um funcionário cadastrado com este código'
+                },
+                validate: {
+                    notNull: {
+                        msg: 'O código é obrigatório'
+                    },
+                    notEmpty: {
+                        msg: 'O código não pode ser vazio'
+                    }
+                }
+            },
             dataNascimento: {
                 type: DataTypes.DATEONLY,
                 allowNull: false,
@@ -14,19 +46,44 @@ class Funcionario extends Gerente {
                         msg: 'A data de nascimento não pode ser vazia'
                     },
                     isDate: {
-                        msg: 'A data de nascimento deve estar no formato YYYY-MM-DD'
+                        msg: 'A data de nascimento deve ser uma data válida'
+                    }
+                }
+            },
+            senha: {
+                type: DataTypes.STRING,
+                allowNull: false,
+                validate: {
+                    notNull: {
+                        msg: 'A senha é obrigatória'
+                    },
+                    notEmpty: {
+                        msg: 'A senha não pode ser vazia'
+                    },
+                    len: {
+                        args: [6, 20],
+                        msg: 'A senha deve ter entre 6 e 20 caracteres'
                     }
                 }
             },
             foto: {
                 type: DataTypes.BLOB('long'),
                 allowNull: true
+            },
+            gerente: {
+                type: DataTypes.BOOLEAN,
+                allowNull: false,
+                defaultValue: false
             }
         }, {
             sequelize,
             modelName: 'funcionario',
             tableName: 'funcionarios'
         });
+    }
+
+    static associate(models) {
+
     }
 }
 
