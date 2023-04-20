c8d3574c74750aa279f856df9b9b5c4bb6f6e718
Sandro Smarzaro <sansmarzaro@gmail.com>
2023-04-19 23:39:52 -0300

    [feat]: add call to all routes in server

--- src/server.js
+++ src/server.js
@@ -1,7 +1,10 @@
 import express from "express";
 import sequelize from "./config/connection.js";
-
+import routes from "./routes.js";
 const app = express();
+
+app.use(express.json());
+app.use("/api/v1", routes);
 
 app.listen(3000, () => {
     console.log("Server is running on port 3000");
