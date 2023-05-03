79792a3e7779e8170a6fc5ded7aebd492c8f32bf
Sandro Smarzaro <sansmarzaro@gmail.com>
2023-05-02 22:03:35 -0300

    [feat]: add use of error handler in server

--- src/server.js
+++ src/server.js
@@ -1,10 +1,13 @@
 import express from "express";
 import sequelize from "./config/connection.js";
 import routes from "./routes.js";
+import errorHandler from "./middlewares/errorHandler.js";
+
 const app = express();
 
 app.use(express.json());
 app.use("/api/v1", routes);
+app.use(errorHandler);
 
 app.listen(3000, () => {
     console.log("Server is running on port 3000");
