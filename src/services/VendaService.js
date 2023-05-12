028df5443ac0c8e8ae55438dc3196b9c942f44c6
Sandro Santiago Smarzaro <sansmarzaro@gmail.com>
2023-05-12 11:44:10 -0300

    [feat]: add business rules of venda

--- src/services/VendaService.js
+++ src/services/VendaService.js
@@ -1,4 +1,8 @@
 import { Venda } from '../models/Venda.js';
+import { CervejaService } from "./CervejaService";
+import { ClienteService } from "./ClienteService";
+
+import sequelize from '../config/database-connection.js';
 
 class VendaService {
     static async index() {
@@ -60,6 +64,58 @@ class VendaService {
         }
         catch (err) {
             throw "Não foi possível deletar a venda";
+        }
+    }
+
+    static async applyBussinessRules(req) {
+        const {
+            dataHora,
+            totalSemCasco,
+            totalComCasco,
+            clienteId,
+            funcionarioId,
+        } = req.body;
+
+        if (await isEmpityStockAvailable()) {
+            await tryApplyDevolutionDiscount(clienteId);
+        }
+        else {
+            throw "Não há espaço nas grades disponível";
+        }
+    }
+
+    static async isEmpityStockAvailable() {
+        const devolutions = await sequelize.query(
+            "SELECT * FROM devolucoes WHERE vendaId = :vendaId",
+            { replacements: { vendaId: this.id }, type: sequelize.QueryTypes.SELECT }
+        );
+        for (const devolution of devolutions) {
+            const cerveja = await CervejaService.show(devolution.cervejaId);
+            if (!cerveja) {
+                throw "Cerveja não encontrada";
+            }
+            const qtdTotalCascos = cerveja.qtdCheio + cerveja.qtdVazio;
+            const qtdAvailableCascos = cerveja.qtdMaxEstoque - qtdTotalCascos;
+            if (devolution.quantidade > qtdAvailableCascos) {
+                throw "Não há espaço nas grades disponível";
+            }
+        }
+    }
+
+    static async tryApplyDevolutionDiscount(clienteId) {
+        const cliente = await ClienteService.show(clienteId, { include: { all: true, nested: true } });
+        if (!cliente) {
+            throw "Cliente não encontrado";
+        }
+        const twoBoxesQtd = 24;
+        if (cliente.qtdCascosDevolvidos >= twoBoxesQtd) {
+            const discountPercentage = 0.05;
+            const discountValueSemCasco = this.totalSemCasco * discountPercentage;
+            this.totalSemCasco -= discountValueSemCasco;
+            const discountValueComCasco = this.totalComCasco * discountPercentage;
+            this.totalComCasco -= discountValueComCasco;
+            cliente.qtdCascosDevolvidos -= twoBoxesQtd;
+            await ClienteService.updateParcial(clienteId, { qtdCascosDevolvidos: cliente.qtdCascosDevolvidos });
         }
     }
 }
