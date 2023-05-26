4ad298bb031d0323eb9e04fd9f795d9bccc2575d
Sandro Santiago Smarzaro <sansmarzaro@gmail.com>
2023-05-26 12:05:49 -0300

    [test]: initial unit tests cases of business rules

---
+++ src/tests/BusinessRulesTests.js
@@ -0,0 +1,20 @@
+import app from '../server.js';
+import axios from 'axios';
+import jest from 'jest';
+
+test ('should don\'t sale without beer in stock', async () => {
+
+});
+
+test ('should don\'t sale without bottle stock avaible', async () => {
+
+});
+
+test ('should sale without discount', async () => {
+
+});
+
+test ('should sale with discount', async () => {
+
+});
+
