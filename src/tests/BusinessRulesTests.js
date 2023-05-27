37094070a877958a7530011df6a530a37dad0462
Sandro Smarzaro <sansmarzaro@gmail.com>
2023-05-26 22:31:47 -0300

    [test]: add 'should don't sale without beer in stock' implementation

--- src/tests/BusinessRulesTests.js
+++ src/tests/BusinessRulesTests.js
@@ -2,8 +2,35 @@ import app from '../server.js';
 import axios from 'axios';
 import jest from 'jest';
 
-test ('should don\'t sale without beer in stock', async () => {
+const uri = 'http://localhost:3000/venda';
 
+test ('should don\'t sale without beer in stock', async () => {
+        const body = {
+            dataHora: "2020-01-01T03:00:00.000Z",
+            totalSemCasco: 9,
+            totalComCasco: 7,
+            clienteId: 4,
+            funcionarioId: 1,
+            itensVenda: [
+                {
+                    quantidade: 1,
+                    valorCerveja: 7,
+                    valorCasco: 2,
+                    cervejaId: 1
+                }
+            ],
+            devolucoes: [
+                {
+                    quantidade: 2,
+                    valorCasco: 2,
+                    cervejaId: 1
+                }
+            ]
+        };
+        const response = await axios.post(uri, body);
+
+        expect(response.status).toBe(400);
+        expect(response.data).toBe('Não foi possível criar a venda: Não há espaço nas grades disponível');
 });
 
 test ('should don\'t sale without bottle stock avaible', async () => {
