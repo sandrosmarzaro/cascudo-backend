96a2ff8d441e1a5a41a1a29648e4496a99207f88
Sandro Smarzaro <sansmarzaro@gmail.com>
2023-06-25 19:07:19 -0300

    [deploy]: add postgre production db config

--- src/config/db.js
+++ src/config/db.js
@@ -1,4 +1,4 @@
-export const dbConfig = {
+const sqliteLocal= {
     dialect: "sqlite",
     storage: "db.sqlite",
     define: {
@@ -7,3 +7,22 @@ export const dbConfig = {
         underscored: true
     }
 };
+
+const postgresqlLocal = {
+    dialect: "postgres",
+    host: "dpg-ci6s80mnqql0ldcrqtkg-a.oregon-postgres.render.com",
+    username: "cascudo_user",
+    password: "cerkrvrCP9zB26DFbDZaEeBVSi0Wr9fO",
+    database: "cascudo_db",
+    define: {
+        timestamps: true,
+        freezeTableName: true,
+        underscored: true
+    },
+    dialectOptions: {
+        ssl: true
+    }
+};
+
+const dbConfig = postgresqlLocal;
+export { dbConfig };
