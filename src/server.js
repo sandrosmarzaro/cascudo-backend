f4a16e06c65ee2ff8994e11055c4a4804d5a7188
Sandro Smarzaro <sansmarzaro@gmail.com>
2023-06-25 16:26:29 -0300

    [style]: remove comment in server

--- src/server.js
+++ src/server.js
@@ -4,7 +4,6 @@ import errorHandler from "./middlewares/errorHandler.js";
 
 const app = express();
 
-// Cabeçalhos adicionados antes que as rotas sejam definidas
 app.use(function (req, res, next) {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
